110b18720a4dd9209a2a8d798927f77661473892
Milky Raven <heartonllamas@gmail.com>
2022-10-29 17:45:18 +0200

    adding layout

--- app.js
+++ app.js
@@ -12,18 +12,40 @@ app.use(express.static("public"));
 
 hbs.registerPartials(__dirname + "/views/partials")
 
-
 app.get("/", async (req, res) => {
+  try{
+    //const response = await axios.get(`https://ghibliapi.herokuapp.com/films`)
+    //const films = response.data
+    res.render("index")
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+app.get("/film-list", async (req, res) => {
     try{
       const response = await axios.get(`https://ghibliapi.herokuapp.com/films`)
       const films = response.data
 
       console.log(films)
-      res.render("index", {films})
+      res.render("film-list", {films})
     } catch (err) {
       console.log(err)
     }
   })
+
+  app.get("/random", async (req, res) => {
+    try{
+      const response = await axios.get(`https://ghibliapi.herokuapp.com/people`)
+      const people = response.data
+
+      console.log(people)
+      res.render("random", {people})
+    } catch (err) {
+      console.log(err)
+    }
+  })
+
 
 
 
